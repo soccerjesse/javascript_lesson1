cb83f35dcc81f980f210c892afaf48b1f1c5899a
soccerjesse <jishperu10@gmail.com>
2022-05-12 22:41:33 +0900

    lesson2

--- js/lesson.js
+++ js/lesson.js
@@ -1,18 +1,9 @@
 'use strict';
 
 { 
-  document.querySelector('button').addEventListener('click', () => {
-    const colors = document.querySelectorAll('input');
-    let selectedcolor;
-
-    colors.forEach(color => {
-      if(color.checked === true){
-        selectedcolor = color.value;
-      }
-    });
-
-    const linode = document.createElement('li');
-    linode.textContent = selectedcolor;
-    document.querySelector('ul').appendChild(linode);
-  });
+  document.querySelector('ul').addEventListener('click', e => {
+    if(e.target.nodeName === 'LI'){
+      e.target.classList.toggle('done');
+    }
+  })
 }
